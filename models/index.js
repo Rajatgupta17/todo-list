04f42ed3b3f4c479e02d364f37f936eec939b785
Rajat Gupta <rajatgupta@Rajats-MBP.domain.name>
2020-08-18 18:34:10 +0530

    database

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose=require("mongoose");
 mongoose.set("debug", true);
-mongoose.connect("mongodb://localhost/todo-api",{ useNewUrlParser: true , useUnifiedTopology:true} );
+mongoose.connect("mmongodb+srv://rajat456bansal:Rajat%401705@cluster0-w5iyn.mongodb.net/todo-list?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology:true} );
 
 
 mongoose.Promise=Promise;
